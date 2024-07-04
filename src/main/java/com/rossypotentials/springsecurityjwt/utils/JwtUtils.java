f64e16727a2995f8df77b5af5ffd597a909873b5
rossypotentials <urokorosemarynnena@gmail.com>
2024-07-04 05:07:11 +0100

    Update

--- src/main/java/com/rossypotentials/springsecurityjwt/utils/JwtUtils.java
+++ src/main/java/com/rossypotentials/springsecurityjwt/utils/JwtUtils.java
@@ -17,18 +17,28 @@ import java.util.function.Function;
 @Component
 
 public class JwtUtils {
- private SecretKey secretKey;
+
+    private SecretKey secretKey;
 
     @Value("${jwt.expiration}")
-    private Long EXPIRATION_TIME;
+    private Long EXPIATION_TIME;
+    private final static String SECRET_KEY =
+            "JpLx8hyycP9RwoEJ+0sSj3p4xsIBmfYe4vVbequytgVfTqXN93NcaTlAVo9y3fpC" +
+                    "" +
+                    "DstegCKTDKFcU30iPKiRbQ==";
+//
+//    @Value("${jwt.secretKey}")
+//    public void setSecretKey(String secretKey) {
+//        byte[] keyBytes = Base64.getDecoder().decode(secretKey.getBytes(StandardCharsets.UTF_8));
+//        this.secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+//    }
 
-    @Value("${jwt.secretKey}")
-    public void setSecretKey(String secretKey) {
-        byte[] keyBytes = Base64.getDecoder().decode(secretKey.getBytes(StandardCharsets.UTF_8));
+
+
+    public JwtUtils(){
+        byte[] keyBytes = Base64.getDecoder().decode(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
         this.secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
     }
-   
-    
 
     public String generateToken(UserDetails userDetails){
         return Jwts.builder()
